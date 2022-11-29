e168becbc7fdbebdfb10bf664e0ebd8bdff8ca1e
Yami0110 <104424673+Yami0110@users.noreply.github.com>
2022-11-29 21:11:15 +0100

    Add files

---
+++ Catalog/delete_media.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once "db_connect.php";
+$db = $GLOBALS['db'];
+
+$mediaDetails = $db->getMediaDetails($_GET['id']);
+$type = $mediaDetails[0]['type_id'];
+
+if (isset($_GET['id'])) {
+    $delete = $db->deleteMediaItem($_GET['id']);
+
+    if ($delete) {
+        if ($type == 1) {
+            header("Location: list.php?mediaType=videos");
+        } else {
+            header("Location: list.php?mediaType=photos");
+        }
+    } else {
+        echo "FATAL ERROR!!!";
+    }
+} else {
+    if ($type == 2) {
+        header("Location: list.php?mediaType=videos");
+    } else {
+        header("Location: list.php?mediaType=photos");
+    }
+}
